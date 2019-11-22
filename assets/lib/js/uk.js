138c84c985ab0a51ecda56004714f61a8f5a71ac
chukyosung <38411970+chukyosung@users.noreply.github.com>
2019-11-22 15:09:38 +0900

    - 메인 info 재작업 - header progress 적용

--- assets/lib/js/uk.js
+++ assets/lib/js/uk.js
@@ -3,10 +3,13 @@ var respon_size = 767;
 var $html = $('html');
 var $body = $('body');
 var $htmlbody = $('html, body');
+var $wrap= $('.wrap');
 
 var $ukHeader = $('.uk_header');
 var $ukFooter = $('.uk_footer');
 var $ukContainer = $('.uk_container');
+
+var hd_progress = 'hd_progress';
 
 var mainHeightArr = [];
 var $main_content = $('.main_content');
@@ -22,6 +25,12 @@ var mainInfoStep5 = $main_info.find('.step5');
 //$(document).ready(function(){
 $(function(){
 
+	//header progress bar 생성
+	$ukHeader.addClass(device_check);
+	$ukHeader.append('<span class="'+hd_progress+'">스크롤 진행상태</span>');
+
+
+	//네비
 	nav_area();
 
 
@@ -29,6 +38,8 @@ $(function(){
 	$(window).resize(function(){
 		var win_w = $(window).width();
 		var win_h = $(window).height();
+		var document_h = $(document).height();
+
 
 		//반응형 pc
 		if( win_w > respon_size ){
@@ -39,18 +50,24 @@ $(function(){
 			$html.addClass('mb').removeClass('pc');
 		}
 
+
 		//main intro height
 		if( device_check === 'device' ) $main_intro.css('height',win_h);
-		//$main_info.css('height',win_h - $ukHeader.height());
+		//if( device_check === 'device' ) $html.css('width',win_w);
 
 
 		//scroll
 		$(window).scroll(function(){
 			var sct = $(window).scrollTop();
 
+			//uk_header ---------------------------//
 			//uk_header fixed css
 			if( sct > 10 ) $ukHeader.addClass('fixed');
 			else $ukHeader.removeClass('fixed');
+			//uk_header progress
+			var docHeight = document_h - win_h;
+			var hd_proW = (sct/docHeight)*100;  //Math.ceil();
+			$('.'+hd_progress).css('width',hd_proW+'%');
 
 			//main info scroll
 			var info_top = $main_intro.height() - $ukHeader.height();
@@ -63,23 +80,12 @@ $(function(){
 			mainInfoStep4.css('transform','translateY('+ (f_sct*-0.2) +'px)');
 			mainInfoStep5.css('transform','translateY('+ (f_sct*-0.1) +'px)');
 
-
 		}).trigger('scroll');
 	}).trigger('resize');
 
 
-
-
-
 	//main_action
 	main_action();
-
-
-
-
-
-
-
 });
 
 
@@ -99,23 +105,10 @@ function nav_area(){
 }
 
 function main_action(){
-	$('.next_content').click(function(){
+	$('.next_content').on('click', function(){
 		var topSize = $main_intro.height() - $ukHeader.height();
-		$htmlbody.stop().animate({'scrollTop':topSize}, 800, 'easeInOutExpo');
-		console.log('aaa');
+		$html.stop().animate({'scrollTop':topSize}, 800, 'easeInOutExpo');
 		return false;
-	});
-}
-
-
-
-function uk_logo(){
-	var $uxkm_logo = $('.uk_logo');
-	$uxkm_logo.each(function(i, e){
-		var uxkm_color = $(e).attr('data-color');
-		$(e).css('color',uxkm_color).append('<span style="background:'+uxkm_color+';">');
-		var fSize = $(e).width() * 0.1;
-		$(e).css({'font-size':fSize, 'opacity':1});
 	});
 }
 
